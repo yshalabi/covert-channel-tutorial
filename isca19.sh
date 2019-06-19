20b234dac1f641a5eca9130f6ed3ca1e5cb8e85e
Yasser Shalabi <yassershalabi@gmail.com>
2019-06-19 16:46:41 -0500

    isca19.sh, needs testing

--- isca19.sh
+++ isca19.sh
@@ -1,7 +1,13 @@
 echo "ISCA 2019 -- Side-Channel Tutorial -- Hands On Session!"
-BINS_DIR=/isca19/bins
-mkdir -p ${BINS_DIR}/send
-mkdir -p ${BINS_DIR}/recv
-cp build/*send* ${BINS_DIR}/send
-cp build/*recv* ${BINS_DIR}/recv
+BINS_DIR=/isca19/bins/channels
+mkdir -p ${BINS_DIR}/channels/Prime+Probe_L1D
+mkdir -p ${BINS_DIR}/channels/Prime+Probe_LLC
+mkdir -p ${BINS_DIR}/channels/Flush+Reload
+mkdir -p ${BINS_DIR}/tools/contention
+cp build/pp_l1d_send ${BINS_DIR}/Prime+Probe_L1D/send
+cp build/pp_l1d_recv ${BINS_DIR}/Prime+Probe_L1D/recv
+cp build/pp_llc_send ${BINS_DIR}/Prime+Probe_LLC/send
+cp build/pp_llc_recv ${BINS_DIR}/Prime+Probe_LLC/recv
+cp build/fr_send ${BINS_DIR}/Flush+Reload/send
+cp build/fr_recv ${BINS_DIR}/Flush+Reload/recv
 chown -R 1000:1000 ${BINS_DIR}
